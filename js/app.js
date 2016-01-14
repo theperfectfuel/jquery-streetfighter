d418eb1564dab2f920954ad8694c81de54fa434a
theperfectfuel <robert@theperfectfuel.com>
2016-01-13 20:10:32 -0800

    Finished hover and click events. Finished hadouken animation and sound.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
 $(document).ready(function() {
-	$("#main-container")
-		$( ".ryu-still" ).mouseenter(function() {
-  $( ".ryu" ).show(".ryu-ready");
+	$(".ryu").mouseenter(function() {
+		$(".ryu-still").hide();
+		$(".ryu-ready").show();
+	});
+	$(".ryu").mouseleave(function() {
+		$(".ryu-still").show();
+		$(".ryu-ready").hide();
+	});
+	$(".ryu").mousedown(function() {
+		playHadouken();
+		$(".ryu-throwing").show();
+		$(".ryu-ready").hide();
+		$(".hadouken").finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+		);
+	});
+	$(".ryu").mouseup(function() {
+		$(".ryu-throwing").hide();
+		$(".ryu-ready").show();
+	});
 });
-});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
